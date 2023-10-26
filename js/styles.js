ce46a474958a038d646cb5e516a30d2a751a7757
Harsh <hp92448@gmail.com>
2023-10-26 13:45:08 +0530

    some changes

--- js/styles.js
+++ js/styles.js
@@ -5,3 +5,47 @@ $(window).scroll(function () {
     $(".cm-navbar-main").removeClass("sticky");
   }
 });
+
+// // fixed card
+// if (window.innerWidth > 991) {
+//   var card = $(".profile-card");
+//   profileCardFixed = "profile-card-fixed";
+//   profileCard = $(".banner-detail-main").height();
+
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > profileCard) {
+//       card.addClass(profileCardFixed);
+//     } else {
+//       card.removeClass(profileCardFixed);
+//     }
+//   });
+// }
+
+// open pdf
+function pdfOPen() {
+  window.open(
+    "https://drive.google.com/file/d/1oHOTRvqJqUbGlbcZ44CBNuLhpnFBxzun/view",
+    "_blank"
+  );
+}
+
+// progress bar
+$(".skill-per").each(function () {
+  var $this = $(this);
+  var per = $this.attr("per");
+  $this.css("width", per + "%");
+  $({ animatedValue: 0 }).animate(
+    { animatedValue: per },
+    {
+      duration: 1000,
+      step: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+      complete: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+    }
+  );
+});
+
+//
