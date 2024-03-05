c0f08015225b8ed167aefb801e1cf6149f16ec7c
Harsh <hp92448@gmail.com>
2024-03-05 17:31:58 +0530

    responcive changes

--- js/styles.js
+++ js/styles.js
@@ -1,37 +1,43 @@
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 1) {
-    $(".cm-navbar-main").addClass("sticky");
-  } else {
-    $(".cm-navbar-main").removeClass("sticky");
-  }
-});
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() > 1) {
+//     $(".cm-navbar-main").addClass("sticky");
+//   } else {
+//     $(".cm-navbar-main").removeClass("sticky");
+//   }
+// });
 
-// fixed card
-if (window.innerWidth > 991) {
-  var card = $(".profile-card");
-  profileCardFixed = "profile-card-fixed";
-  profileCard = $(".banner-detail-main").height();
-  var timelineWrap = $(".timeline-wrap");
-  timelineMain = $(".timeline-main").height();
+// $(document).ready(function () {
+//   console.log(window.innerWidth, "window.innerWidth");
+//   // fixed card
+//   if (window.innerWidth > 991) {
+//     var card = $(".profile-card");
+//     profileCardFixed = "profile-card-fixed";
+//     profileCard = $(".banner-detail-main").height();
+//     var timelineWrap = $(".timeline-wrap");
+//     timelineMain = $(".timeline-main").height();
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > profileCard) {
-      card.addClass(profileCardFixed);
-    } else {
-      card.removeClass(profileCardFixed);
-    }
-    if ($(document).scrollTop() > timelineMain) {
-      timelineWrap.addClass("timelineAnimation");
-    } else {
-      timelineWrap.removeClass("timelineAnimation");
-    }
-    if ($(document).scrollTop() > timelineMain) {
-      timelineWrap.addClass("timelineAnimation");
-    } else {
-      timelineWrap.removeClass("timelineAnimation");
-    }
-  });
-}
+//     $(window).scroll(function () {
+//       if ($(this).scrollTop() > profileCard) {
+//         card.addClass(profileCardFixed);
+//       } else {
+//         card.removeClass(profileCardFixed);
+//       }
+//       if ($(document).scrollTop() > timelineMain) {
+//         timelineWrap.addClass("timelineAnimation");
+//       } else {
+//         timelineWrap.removeClass("timelineAnimation");
+//       }
+//       if ($(document).scrollTop() > timelineMain) {
+//         timelineWrap.addClass("timelineAnimation");
+//       } else {
+//         timelineWrap.removeClass("timelineAnimation");
+//       }
+//     });
+//   } else {
+//     var card = $(".profile-card");
+//     card.removeClass("profile-card-fixed");
+//   }
+// });
 
 // open pdf
 function pdfOPen() {
@@ -62,17 +68,4 @@ $(".skill-per").each(function () {
 });
 // }
 
-// Form data receive on gmail
-function sendEmail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "hp92448@gmail.com",
-    Password: "57669684233DCF2A92702CEFAB9CB8CE8860",
-    To: "hp92448@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "test",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
-
-AOS.init();
+// AOS.init();
